d632e4737e8847e8241d35c3dbbdcf27b2763b27
Luis Rodriguez <106412395+Luis-Rodriguez-02@users.noreply.github.com>
2024-12-15 20:46:15 -0500

    finish operator functions

---
+++ script.js
@@ -0,0 +1,22 @@
+// Operator Functions
+function add(a, b) {
+  return (a + b);
+}
+
+function subtract(a, b) {
+ return (a - b);
+}
+
+function multiply(a, b) {
+  return (a * b);
+}
+
+function divde(a, b) {
+  if (b === 0) {
+    return `Nice try -> cannot divide by zero`;
+  }
+  else {
+    return (a / b);
+  }
+}
+
