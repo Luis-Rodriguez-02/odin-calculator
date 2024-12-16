8f67829a82b556d10110622b393ce856890b4463
Luis Rodriguez <106412395+Luis-Rodriguez-02@users.noreply.github.com>
2024-12-15 20:58:53 -0500

    finish operate function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Operator Functions
+// OPERATOR FUNCTIONS
 function add(a, b) {
   return (a + b);
 }
@@ -11,12 +11,30 @@ function multiply(a, b) {
   return (a * b);
 }
 
-function divde(a, b) {
+function divide(a, b) {
   if (b === 0) {
     return `Nice try -> cannot divide by zero`;
   }
-  else {
-    return (a / b);
-  }
+  return (a / b);
 }
 
+// GLOBAL OPERATION VARIABLES
+let leftOperand = 0;
+let rightOperand = 0;
+let operator = null;
+
+// OPERATE FUNCTION
+function operate(leftNum, operator, rightNum) {
+  switch(operator) {
+    case "+":
+      return add(leftNum, rightNum);
+    case "-":
+      return subtract(leftNum, rightNum);
+    case "*":
+      return multiply(leftNum, rightNum);
+    case "/":
+      return divide(leftNum, rightNum);
+    default:
+      return `Invalid operator`;
+  }
+}
